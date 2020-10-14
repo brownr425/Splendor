bf169af0c9617e6e70b2696ff198c1a9240dd842
Aidan Day <day22@up.edu>
2020-10-14 14:33:08 -0700

    started implementing deep copy and added getters

--- app/src/main/java/edu/up/cs301/splendor/Card.java
+++ app/src/main/java/edu/up/cs301/splendor/Card.java
@@ -3,6 +3,7 @@ package edu.up.cs301.splendor;
 public class Card {
     private int rPrice;
     private int bPrice;
+
     private int gPrice;
     private int wPrice;
     private int brPrice;
@@ -11,5 +12,28 @@ public class Card {
     private int cardLevel;
     private int prestigePoints;
 
+
+    public Card(int rPrice, int bPrice, int gPrice, int wPrice, int brPrice, int colorGem, int cardLevel, int prestigePoints) {
+        this.rPrice = rPrice;
+        this.bPrice = bPrice;
+        this.gPrice = gPrice;
+        this.wPrice = wPrice;
+        this.brPrice = brPrice;
+        this.colorGem = colorGem;
+        this.cardLevel = cardLevel;
+        this.prestigePoints = prestigePoints;
+    }
+    
+    //Copy Constructor 
+    public Card(Card cardToCopy) {
+        this.rPrice = cardToCopy.rPrice;
+        this.bPrice = cardToCopy.bPrice;
+        this.gPrice = cardToCopy.gPrice;
+        this.wPrice = cardToCopy.wPrice;
+        this.brPrice = cardToCopy.brPrice;
+        this.colorGem = cardToCopy.colorGem;
+        this.cardLevel = cardToCopy.cardLevel;
+        this.prestigePoints = cardToCopy.prestigePoints;
+    }
 
 }
--- app/src/main/java/edu/up/cs301/splendor/SplendorGameState.java
+++ app/src/main/java/edu/up/cs301/splendor/SplendorGameState.java
@@ -1,12 +1,20 @@
 package edu.up.cs301.splendor;
 
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 
 import edu.up.cs301.game.GameFramework.infoMessage.GameState;
 
 public class SplendorGameState extends GameState {
 
-//~~~~~~~~~~~~~ player names and IDs ~~~~~~~~~~~ //
+
+    //~~~~~~~~~~~~~ player names and IDs ~~~~~~~~~~~ //
     //player display names
     //TODO figure out how to get the player names from super class.
     private String player1Name;
@@ -127,18 +135,126 @@ public class SplendorGameState extends GameState {
 
 //~~~~~~~~~~~~~~~~~~ Deck Information ~~~~~~~~~~~~~~~ //
 
-    private ArrayList<Cards> rank1Stack; //ArrayList of rank1 cards
-    private ArrayList<Cards> rank2Stack; //ArrayList of rank2 cards
-    private ArrayList<Cards> rank3Stack; //ArrayList of rank3 cards
+    private ArrayList<Card> rank1Stack; //ArrayList of rank1 cards
+    private ArrayList<Card> rank2Stack; //ArrayList of rank2 cards
+    private ArrayList<Card> rank3Stack; //ArrayList of rank3 cards
 
     private Noble noble1;
     private Noble noble2;
     private Noble noble3;
     private Noble noble4;
 
+    /*
+    *New Game Constructor
+    * TODO
+    *  -figure out  file reading for initializing deck array lists
+    *  -possibly add noble/card visibility boolean instance variables
+    */
     public SplendorGameState(){
         initializePlayerPointValues();
-        initializeDecks();
+        initializeDecks(); //unfinished
+    }
+
+    /*
+    *
+    * Deep copy constructor
+    * TODO add nobles and card visibility
+    */
+    public SplendorGameState(SplendorGameState newGameState) {
+
+        //deep copy of player 1 points and reserve cards
+        newGameState.p1GoldCoins = this.getP1GoldCoins();
+        newGameState.p1GoldPts = this.getP1GoldPts();
+        newGameState.p1EmeraldCoins = this.getP1EmeraldCoins();
+        newGameState.p1EmeraldPts = this.getP1EmeraldPts();
+        newGameState.p1SapphireCoins = this.getP1SapphireCoins();
+        newGameState.p1SapphirePts = this.getP1SapphirePts();
+        newGameState.p1RubyCoins = this.getP1RubyCoins();
+        newGameState.p1RubyPts = this.getP1RubyPts();
+        newGameState.p1OnyxCoins = this.getP1OnyxCoins();
+        newGameState.p1OnyxPts = this.getP1OnyxPts();
+        newGameState.p1DiamondCoins = this.getP1DiamondCoins();
+        newGameState.p1PrestigePts = this.getP1OnyxPts();
+        newGameState.p1NumCardsReserved = this.getP1NumCardsReserved();
+        newGameState.p1ReserveCards = new ArrayList<Card>();
+        for(Card card: this.p1ReserveCards) {
+            newGameState.p1ReserveCards.add(new Card(card)); //uses copy constructor in card 
+        }
+
+        //deep copy of player 2 points and reserve cards
+        newGameState.p2GoldCoins = this.getP2GoldCoins();
+        newGameState.p2GoldPts = this.getP2GoldPts();
+        newGameState.p2EmeraldCoins = this.getP2EmeraldCoins();
+        newGameState.p2EmeraldPts = this.getP2EmeraldPts();
+        newGameState.p2SapphireCoins = this.getP2SapphireCoins();
+        newGameState.p2SapphirePts = this.getP2SapphirePts();
+        newGameState.p2RubyCoins = this.getP2RubyCoins();
+        newGameState.p2RubyPts = this.getP2RubyPts();
+        newGameState.p2OnyxCoins = this.getP2OnyxCoins();
+        newGameState.p2OnyxPts = this.getP2OnyxPts();
+        newGameState.p2DiamondCoins = this.getP2DiamondCoins();
+        newGameState.p2PrestigePts = this.getP2OnyxPts();
+        newGameState.p2NumCardsReserved = this.getP2NumCardsReserved();
+        newGameState.p2ReserveCards = new ArrayList<Card>();
+        for(Card card: this.p2ReserveCards) {
+            newGameState.p2ReserveCards.add(new Card(card)); //uses copy constructor in card 
+        }
+
+        //deep copy of player 3 points and reserve cards
+        newGameState.p3GoldCoins = this.getP3GoldCoins();
+        newGameState.p3GoldPts = this.getP3GoldPts();
+        newGameState.p3EmeraldCoins = this.getP3EmeraldCoins();
+        newGameState.p3EmeraldPts = this.getP3EmeraldPts();
+        newGameState.p3SapphireCoins = this.getP3SapphireCoins();
+        newGameState.p3SapphirePts = this.getP3SapphirePts();
+        newGameState.p3RubyCoins = this.getP3RubyCoins();
+        newGameState.p3RubyPts = this.getP3RubyPts();
+        newGameState.p3OnyxCoins = this.getP3OnyxCoins();
+        newGameState.p3OnyxPts = this.getP3OnyxPts();
+        newGameState.p3DiamondCoins = this.getP3DiamondCoins();
+        newGameState.p3PrestigePts = this.getP3OnyxPts();
+        newGameState.p3NumCardsReserved = this.getP3NumCardsReserved();
+        newGameState.p3ReserveCards = new ArrayList<Card>();
+        for(Card card: this.p3ReserveCards) {
+            newGameState.p3ReserveCards.add(new Card(card)); //uses copy constructor in card 
+        }
+
+        //deep copy of player 4 points and reserve cards
+        newGameState.p4GoldCoins = this.getP4GoldCoins();
+        newGameState.p4GoldPts = this.getP4GoldPts();
+        newGameState.p4EmeraldCoins = this.getP4EmeraldCoins();
+        newGameState.p4EmeraldPts = this.getP4EmeraldPts();
+        newGameState.p4SapphireCoins = this.getP4SapphireCoins();
+        newGameState.p4SapphirePts = this.getP4SapphirePts();
+        newGameState.p4RubyCoins = this.getP4RubyCoins();
+        newGameState.p4RubyPts = this.getP4RubyPts();
+        newGameState.p4OnyxCoins = this.getP4OnyxCoins();
+        newGameState.p4OnyxPts = this.getP4OnyxPts();
+        newGameState.p4DiamondCoins = this.getP4DiamondCoins();
+        newGameState.p4PrestigePts = this.getP4OnyxPts();
+        newGameState.p4NumCardsReserved = this.getP4NumCardsReserved();
+        newGameState.p4ReserveCards = new ArrayList<Card>();
+        for(Card card: this.p4ReserveCards) {
+            newGameState.p4ReserveCards.add(new Card(card)); //uses copy constructor in card 
+        }
+        
+        //deep copies for all 3 card stacks
+        newGameState.rank1Stack = new ArrayList<>();
+        for(Card rankCard: this.rank1Stack) {
+            newGameState.rank1Stack.add(new Card(rankCard)); //uses copy constructor in card 
+        }
+
+        newGameState.rank2Stack = new ArrayList<>();
+        for(Card rankCard: this.rank2Stack) {
+            newGameState.rank2Stack.add(new Card(rankCard)); //uses copy constructor in card 
+        }
+
+        newGameState.rank3Stack = new ArrayList<>();
+        for(Card rankCard: this.rank3Stack) {
+            newGameState.rank3Stack.add(new Card(rankCard)); //uses copy constructor in card 
+        }
+
+        //TODO make noble class and copy constructor
     }
 
     //helper method for constructor setting all point values for player to zero
@@ -205,10 +321,318 @@ public class SplendorGameState extends GameState {
         this.p4ReserveCards = new ArrayList<Card>();
     }
 
+    //TODO file reading from three rank text files
+    /*initialize Decks
+    * reads input from text files into three array lists then shuffles deck
+    *
+    */
     public void initializeDecks() {
         this.rank1Stack = new ArrayList<Card>();
         this.rank2Stack = new ArrayList<Card>();
         this.rank3Stack = new ArrayList<Card>();
         
+    }
+    public String getPlayer1Name() {
+        return player1Name;
+    }
+
+    public String getPlayer2Name() {
+        return player2Name;
+    }
+
+    public String getPlayer3Name() {
+        return player3Name;
+    }
+
+    public String getPlayer4Name() {
+        return player4Name;
+    }
+
+    public int getPlayerTurn() {
+        return playerTurn;
+    }
+
+    public int getPLAYER1ID() {
+        return PLAYER1ID;
+    }
+
+    public int getPLAYER2ID() {
+        return PLAYER2ID;
+    }
+
+    public int getPLAYER3ID() {
+        return PLAYER3ID;
+    }
+
+    public int getPLAYER4ID() {
+        return PLAYER4ID;
+    }
+
+    public int getP1PrestigePts() {
+        return p1PrestigePts;
+    }
+
+    public int getP1GoldPts() {
+        return p1GoldPts;
+    }
+
+    public int getP1EmeraldPts() {
+        return p1EmeraldPts;
+    }
+
+    public int getP1SapphirePts() {
+        return p1SapphirePts;
+    }
+
+    public int getP1RubyPts() {
+        return p1RubyPts;
+    }
+
+    public int getP1DiamondPts() {
+        return p1DiamondPts;
+    }
+
+    public int getP1OnyxPts() {
+        return p1OnyxPts;
+    }
+
+    public int getP1GoldCoins() {
+        return p1GoldCoins;
+    }
+
+    public int getP1EmeraldCoins() {
+        return p1EmeraldCoins;
+    }
+
+    public int getP1SapphireCoins() {
+        return p1SapphireCoins;
+    }
+
+    public int getP1RubyCoins() {
+        return p1RubyCoins;
+    }
+
+    public int getP1DiamondCoins() {
+        return p1DiamondCoins;
+    }
+
+    public int getP1OnyxCoins() {
+        return p1OnyxCoins;
+    }
+
+    public int getP1NumCardsReserved() {
+        return p1NumCardsReserved;
+    }
+
+    public ArrayList<Card> getP1ReserveCards() {
+        return p1ReserveCards;
+    }
+
+    public int getP2PrestigePts() {
+        return p2PrestigePts;
+    }
+
+    public int getP2GoldPts() {
+        return p2GoldPts;
+    }
+
+    public int getP2EmeraldPts() {
+        return p2EmeraldPts;
+    }
+
+    public int getP2SapphirePts() {
+        return p2SapphirePts;
+    }
+
+    public int getP2RubyPts() {
+        return p2RubyPts;
+    }
+
+    public int getP2DiamondPts() {
+        return p2DiamondPts;
+    }
+
+    public int getP2OnyxPts() {
+        return p2OnyxPts;
+    }
+
+    public int getP2GoldCoins() {
+        return p2GoldCoins;
+    }
+
+    public int getP2EmeraldCoins() {
+        return p2EmeraldCoins;
+    }
+
+    public int getP2SapphireCoins() {
+        return p2SapphireCoins;
+    }
+
+    public int getP2RubyCoins() {
+        return p2RubyCoins;
+    }
+
+    public int getP2DiamondCoins() {
+        return p2DiamondCoins;
+    }
+
+    public int getP2OnyxCoins() {
+        return p2OnyxCoins;
+    }
+
+    public int getP2NumCardsReserved() {
+        return p2NumCardsReserved;
+    }
+
+    public ArrayList<Card> getP2ReserveCards() {
+        return p2ReserveCards;
+    }
+
+    public int getP3PrestigePts() {
+        return p3PrestigePts;
+    }
+
+    public int getP3GoldPts() {
+        return p3GoldPts;
+    }
+
+    public int getP3EmeraldPts() {
+        return p3EmeraldPts;
+    }
+
+    public int getP3SapphirePts() {
+        return p3SapphirePts;
+    }
+
+    public int getP3RubyPts() {
+        return p3RubyPts;
+    }
+
+    public int getP3DiamondPts() {
+        return p3DiamondPts;
+    }
+
+    public int getP3OnyxPts() {
+        return p3OnyxPts;
+    }
+
+    public int getP3GoldCoins() {
+        return p3GoldCoins;
+    }
+
+    public int getP3EmeraldCoins() {
+        return p3EmeraldCoins;
+    }
+
+    public int getP3SapphireCoins() {
+        return p3SapphireCoins;
+    }
+
+    public int getP3RubyCoins() {
+        return p3RubyCoins;
+    }
+
+    public int getP3DiamondCoins() {
+        return p3DiamondCoins;
+    }
+
+    public int getP3OnyxCoins() {
+        return p3OnyxCoins;
+    }
+
+    public int getP3NumCardsReserved() {
+        return p3NumCardsReserved;
+    }
+
+    public ArrayList<Card> getP3ReserveCards() {
+        return p3ReserveCards;
+    }
+
+    public int getP4PrestigePts() {
+        return p4PrestigePts;
+    }
+
+    public int getP4GoldPts() {
+        return p4GoldPts;
+    }
+
+    public int getP4EmeraldPts() {
+        return p4EmeraldPts;
+    }
+
+    public int getP4SapphirePts() {
+        return p4SapphirePts;
+    }
+
+    public int getP4RubyPts() {
+        return p4RubyPts;
+    }
+
+    public int getP4DiamondPts() {
+        return p4DiamondPts;
+    }
+
+    public int getP4OnyxPts() {
+        return p4OnyxPts;
+    }
+
+    public int getP4GoldCoins() {
+        return p4GoldCoins;
+    }
+
+    public int getP4EmeraldCoins() {
+        return p4EmeraldCoins;
+    }
+
+    public int getP4SapphireCoins() {
+        return p4SapphireCoins;
+    }
+
+    public int getP4RubyCoins() {
+        return p4RubyCoins;
+    }
+
+    public int getP4DiamondCoins() {
+        return p4DiamondCoins;
+    }
+
+    public int getP4OnyxCoins() {
+        return p4OnyxCoins;
+    }
+
+    public int getP4NumCardsReserved() {
+        return p4NumCardsReserved;
+    }
+
+    public ArrayList<Card> getP4ReserveCards() {
+        return p4ReserveCards;
+    }
+
+    public ArrayList<Card> getRank1Stack() {
+        return rank1Stack;
+    }
+
+    public ArrayList<Card> getRank2Stack() {
+        return rank2Stack;
+    }
+
+    public ArrayList<Card> getRank3Stack() {
+        return rank3Stack;
+    }
+
+    public Noble getNoble1() {
+        return noble1;
+    }
+
+    public Noble getNoble2() {
+        return noble2;
+    }
+
+    public Noble getNoble3() {
+        return noble3;
+    }
+
+    public Noble getNoble4() {
+        return noble4;
     }
 }
