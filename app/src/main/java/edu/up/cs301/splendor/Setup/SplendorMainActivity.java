79c92bc67813f120be7b01148f0c9415c789de50
Aidan Day <day22@up.edu>
2020-12-05 00:16:06 -0800

    changed max players to 2

--- app/src/main/java/edu/up/cs301/splendor/Setup/SplendorMainActivity.java
+++ app/src/main/java/edu/up/cs301/splendor/Setup/SplendorMainActivity.java
@@ -57,7 +57,7 @@ public class SplendorMainActivity extends GameMainActivity {
         // - from 1 to 2 players
         // - name of game is "Counter Game"
         // - port number as defined above
-        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 4, "Splendor Game",
+        GameConfig defaultConfig = new GameConfig(playerTypes, 2, 2, "Splendor Game",
                 PORT_NUMBER);
 
         // Add the default players to the configuration
